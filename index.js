4bdac8438a682a39a5f39d443febb5765c38f841
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-11 23:29:42 +0700

    fix: move express below /webbook for fix bugs 500 from message line api

--- index.js
+++ index.js
@@ -7,22 +7,13 @@ const webhookRoute = require("./routes/webhook.route.js");
 dotenv.config();
 
 
-const app = express();
+
 app.use(express.json()); // global json parser
 app.use(cors())
 
-// const supabase = createClient(
-//   process.env.SUPABASE_URL,
-//   process.env.SUPABASE_KEY
-// );
-// (async()=>{
-//     const {data,error} = await supabase.from('ExpensesList').select('*');
-//     if(error) console.error('Supabase connection error:', error);
-//     else console.log('Supabase connected, expenses:', data);
-// })()
-
-// mount webhook route (route handles its own body parsing after middleware)
 app.use('/webhook', webhookRoute);
+
+const app = express();
 app.use('/api/expenses',expensesRoute);
 
 // optional simple root
