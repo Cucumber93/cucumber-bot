3460337055609053bfde121694dc3fb18838ba2c
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-12 22:10:36 +0700

    fix: seperate net balance value and trend

--- controllers/balance.controller.js
+++ controllers/balance.controller.js
@@ -1,47 +1,13 @@
 const balanceService = require("../services/balance.service.js");
 
-exports.getBalanceByType = async (req, res) => {
+exports.getBalance = async (req, res) => {
   try {
-    const { type } = req.params;
-
-    let result;
-
-    switch (type) {
-      // 🧮 Net Balance (รอบเดือนปัจจุบัน)
-      case "net":
-        result = await balanceService.calculateNetBalance();
-        break;
-
-      // 📅 รวมยอดรายเดือน
-      case "monthly":
-        result = await balanceService.getMonthlySummary();
-        break;
-
-      // 🧾 รายละเอียดรายเดือน (ต้องมี query ?year=2025&month=10)
-      case "monthly-details": {
-        const { year, month } = req.query;
-        if (!year || !month)
-          return res.status(400).json({ error: "Missing year or month query params" });
-
-        result = await balanceService.getMonthlyDetails(Number(year), Number(month));
-        break;
-      }
-
-      // 📆 รวมยอดรายปี
-      case "yearly":
-        result = await balanceService.getYearlySummary();
-        break;
-
-      // ❌ หาก type ไม่ตรง
-      default:
-        return res.status(400).json({
-          error: "Invalid type. Use one of: net, monthly, monthly-details, yearly",
-        });
-    }
-
+    const result = await balanceService.calculateNetBalance();
     res.json(result);
   } catch (err) {
     console.error("❌ Controller error (getBalanceByType):", err.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({
+      error: "Internal Server Error"
+    });
   }
 };
---
+++ controllers/trendIncomeAndExpenses.controller.js
@@ -0,0 +1,43 @@
+const trendIncomeAndExpenses = require('../services/trendIncomeAndExpenses.service')
+
+exports.getBalanceByType = async (req, res) => {
+  try {
+    const { type } = req.params;
+
+    let result;
+
+    switch (type) {
+
+      // 📅 รวมยอดรายเดือน
+      case "monthly":
+        result = await trendIncomeAndExpenses.getMonthlySummary();
+        break;
+
+      // 🧾 รายละเอียดรายเดือน (ต้องมี query ?year=2025&month=10)
+      case "monthly-details": {
+        const { year, month } = req.query;
+        if (!year || !month)
+          return res.status(400).json({ error: "Missing year or month query params" });
+
+        result = await trendIncomeAndExpenses.getMonthlyDetails(Number(year), Number(month));
+        break;
+      }
+
+      // 📆 รวมยอดรายปี
+      case "yearly":
+        result = await trendIncomeAndExpenses.getYearlySummary();
+        break;
+
+      // ❌ หาก type ไม่ตรง
+      default:
+        return res.status(400).json({
+          error: "Invalid type. Use one of: net, monthly, monthly-details, yearly",
+        });
+    }
+
+    res.json(result);
+  } catch (err) {
+    console.error("❌ Controller error (getBalanceByType):", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
