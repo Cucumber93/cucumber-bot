dedb8560e342f936b49d4d65c2288d21bb9b05c2
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-14 02:02:59 +0700

    feature: add router on category income

--- routes/categoriesIncome.route.js
+++ routes/categoriesIncome.route.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const controller = require("../controllers/categoriesIncome.controller.js")
 
 // POST: เพิ่ม Category ผ่าน API
-router.post("/", controller.createCategory)
+router.post("/create", controller.createCategory)
+router.post("/get",controller.getCategoriesByUser)
+router.post("/update",controller.updateCategory)
+router.post("/delete",controller.deleteCategory)
 
 module.exports = router
